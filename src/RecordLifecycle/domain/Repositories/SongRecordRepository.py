06c6a8438b00d1ac4d3dfc323dc58df7383b1d4d
David19 <davidsanchezpalomino200603@gmail.com>
2026-07-17 11:42:38 +0200

    Feat: SongRecordRepository

---
+++ src/RecordLifecycle/domain/Repositories/SongRecordRepository.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from src.RecordLifecycle.domain.Entities.SongRecord import SongRecord
+from src.RecordLifecycle.domain.ValueObjects.AuthorId import AuthorId
+from src.RecordLifecycle.domain.ValueObjects.RecordId import RecordId
+
+class SongRecordRepository(ABC):
+    
+    @abstractmethod
+    def save(self, record: SongRecord):
+        pass
+    
+    @abstractmethod
+    def get_by_id(self, id: RecordId):
+        pass
+    
+    @abstractmethod
+    def delete_by_id(self, id: RecordId):
+        pass
+    
+    @abstractmethod
+    def get_user_records(self, author_id: AuthorId):
+        pass
+    
+    @abstractmethod
+    def update_record(self, record: SongRecord):
+        pass
+    
+    @abstractmethod
+    def get_album_duration(self, album_id: RecordId):
+        pass
