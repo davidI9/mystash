8243e042e5df06a76b8873af57bf5e77a740f232
David19 <davidsanchezpalomino200603@gmail.com>
2026-07-23 17:22:28 +0200

    smol refactor

--- src/RecordLifecycle/infrastructure/Persistance/VideogameRecordRepositoryImpl.py
+++ src/RecordLifecycle/infrastructure/Persistance/VideogameRecordRepositoryImpl.py
@@ -1,6 +1,8 @@
 from pymongo.database import Database
 from src.RecordLifecycle.domain.Repositories.VideogameRecordRepository import VideogameRecordRepository
 from src.RecordLifecycle.domain.Entities.VideogameRecord import VideogameRecord
+from src.RecordLifecycle.domain.ValueObjects.AuthorId import AuthorId
+from src.RecordLifecycle.domain.ValueObjects.RecordId import RecordId
 from .VideogameMongodbRepo import VideogameMongodbRepo
 
 class VideogameRecordRepositoryImpl(VideogameRecordRepository):
@@ -11,13 +13,13 @@ class VideogameRecordRepositoryImpl(VideogameRecordRepository):
     def save(self, record: VideogameRecord):
         self.videogame_records_db.save_in_db(record)
     
-    def get_by_id(self, id):
+    def get_by_id(self, id: RecordId):
         return self.videogame_records_db.get_by_id_from_db(id)
     
-    def delete_by_id(self, id):
+    def delete_by_id(self, id: RecordId):
         return self.videogame_records_db.delete_by_id(id)
     
-    def get_user_records(self, author_id):
+    def get_user_records(self, author_id: AuthorId):
         return self.videogame_records_db.get_user_records(author_id)
     
     def update_record(self, record: VideogameRecord):
