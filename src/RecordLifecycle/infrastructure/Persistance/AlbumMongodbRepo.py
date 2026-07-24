bdbd2c5cc4c329b47d9124e2bf1ae0e2715341f5
David19 <davidsanchezpalomino200603@gmail.com>
2026-07-24 17:27:03 +0200

    feat: mongodb repository for albums fully implemented

--- src/RecordLifecycle/infrastructure/Persistance/AlbumMongodbRepo.py
+++ src/RecordLifecycle/infrastructure/Persistance/AlbumMongodbRepo.py
@@ -23,3 +23,50 @@ class AlbumMongodbRepo:
                 print(f"An error has occurred while saving in MongoDB: {e}")
             
             
+        def get_by_id_from_db(self, id: RecordId):
+            raw_record: dict[str, Any] | None
+            record: AlbumRecord | None
+            
+            try:
+                raw_record = self.album_records.find_one({"record_id": str(id)})
+                record = self.map_record(raw_record)
+                return record
+            
+            except Exception as e:
+                print("An error has ocurred while getting the requested record from MongoDB.")
+                
+        def map_record(self, raw_record):
+            record = AlbumRecord(raw_record["author_id"], raw_record["title"], raw_record["date"], raw_record["record_id"], raw_record["artist"], raw_record["main_genre"])
+            return record
+        
+        def delete_by_id(self, id: RecordId):
+            try:
+                self.album_records.delete_one({"record_id": str(id)})
+                print("Deleted without errors.")
+            except Exception as e:
+                print(e)
+        
+        def get_user_records(self, author_id: AuthorId):
+            raw_records: Cursor
+
+            try:
+                raw_records = self.album_records.find({"author_id": str(author_id)})
+                records = [self.map_record(raw_record) for raw_record in raw_records]
+                return records
+            except Exception as e:
+                print(e)
+                
+        def update_record(self, record: AlbumRecord):
+            try:
+                self.album_records.update_one(
+                    {"record_id": str(record.get_id()), "author_id": str(record.get_author())},
+                    {"$set": {
+                        "title": str(record.get_album_title()),
+                        "date": str(record.get_date()),
+                        "artist": str(record.get_artist()),
+                        "main_genre": str(record.get_main_genre())
+                    }}
+                )
+                print("Updated without errors.")
+            except Exception as e:
+                print(e)
