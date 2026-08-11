69dbd5ba9781324550a6df140aa523cb26bcb28e
David19 <davidsanchezpalomino200603@gmail.com>
2026-08-11 19:07:38 +0200

    feat: main global controller for the monorepo

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+import os
+from src.RecordLifecycle.infrastructure.records_controllers import get_record_router
+
+app = FastAPI()
+mongo_client = MongoClient(os.getenv("MONGODB_URL"))
+app.include_router(get_record_router(mongo_client))
